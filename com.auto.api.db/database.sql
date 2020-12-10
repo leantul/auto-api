fdbdf0d8c70e96f13f6532fd9300da039ff8e2d3
Leandro Tullio <letullio@gmail.com>
2020-12-09 23:25:28 -0300

    Add files via upload
    Se agrega database.sql

---
+++ com.auto.api.db/database.sql
@@ -0,0 +1,14 @@
+create database auto;
+use auto;
+
+#Inserts iniciales tabla additional
+insert into additional(name, price) values ('Techo corredizo', 12000);
+insert into additional(name, price) values ('Aire Acondicionado', 20000);
+insert into additional(name, price) values ('Sistemas de frenos ABS', 14000);
+insert into additional(name, price) values ('Airbag', 7000);
+insert into additional(name, price) values ('Llantas de aleación', 12000);
+
+#Inserts iniciales tabla auto
+insert into auto(name, price) values ('Sedán', 230000);
+insert into auto(name, price) values ('Familiar', 245000);
+insert into auto(name, price) values ('Coupé', 270000);
